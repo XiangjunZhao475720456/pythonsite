62381a9559784c2c8c108b68cd4e47defff47268
XiangjunZhao475720456 <475720456@qq.com>
2018-02-09 17:09:18 +0800

    删除app目录文件

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_bootstrap3 import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
--- app/urls/url.py
+++
@@ -1,3 +0,0 @@
-from app import app
-
-app.add_url_rule(rule='/login',view_func='login')

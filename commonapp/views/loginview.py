6eb5170eb74df05149c2428fbd0761c2a1c400d9
XiangjunZhao475720456 <475720456@qq.com>
2018-02-09 19:19:28 +0800

    ut8编码

--- commonapp/views/loginview.py
+++ commonapp/views/loginview.py
@@ -1,6 +1,8 @@
+#! encoding:utf-8
+
 from flask import render_template
 
 
 def home():
-    content = {'nickname':'Tom'}
-    return render_template('common/index.html',**content)
+    content = {'nickname': 'Tom'}
+    return render_template('common/index.html', **content)

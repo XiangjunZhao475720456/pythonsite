6ce647cfd468404cddfdefa3eb36e136e0918a7b
XiangjunZhao475720456 <475720456@qq.com>
2018-02-12 13:47:16 +0800

    将扩展组件移动到exts.py文件

--- pythonsite.py
+++ pythonsite.py
@@ -1,22 +1,22 @@
 #! encoding:utf-8
 
 from flask import Flask, redirect, url_for
-from flask_bootstrap3 import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_required
 
 from commonapp import common
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
 
 
 @app.route('/')
+@login_required
 def index():
     return redirect(url_for('common.home'))
 
 
 if __name__ == '__main__':
+    from exts import *
+
     app.register_blueprint(common)
     app.run(host='0.0.0.0')

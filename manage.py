8740a60d43339636303d55ea13bf680711fe75cd
XiangjunZhao475720456 <475720456@qq.com>
2018-02-21 17:08:48 +0800

    修改导入User\Role的位置

--- manage.py
+++ manage.py
@@ -3,13 +3,14 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-from commonapp.models.usermodel import User, Role
 from pythonsite import app
 from exts import db
 
 manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
+from commonapp.models.usermodel import User, Role
 
 if __name__ == '__main__':
     manager.run()

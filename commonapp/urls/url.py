18ef69311f50bc1261e0a037dab409154ebb747c
XiangjunZhao475720456 <475720456@qq.com>
2018-03-13 00:28:44 +0800

    添加注册路由

--- commonapp/urls/url.py
+++ commonapp/urls/url.py
@@ -1,6 +1,7 @@
 # coding:UTF-8
 
-from commonapp import common, login, logout
+from commonapp import common, login, logout, regist
 
-common.add_url_rule(rule='/login', endpoint='login', view_func=login, methods=['GET', 'POST'])
-common.add_url_rule(rule='/logout', endpoint='logout', view_func=logout, methods=['GET', 'POST'])
+common.add_url_rule(rule='/regist/', endpoint='regist', view_func=regist, methods=['GET', 'POST'])
+common.add_url_rule(rule='/login/', endpoint='login', view_func=login, methods=['GET', 'POST'])
+common.add_url_rule(rule='/logout/', endpoint='logout', view_func=logout, methods=['GET', 'POST'])

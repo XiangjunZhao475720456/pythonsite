56afd7d8d729fe458d2e1a36e267a86cf0824cf0
XiangjunZhao475720456 <475720456@qq.com>
2018-03-13 00:30:52 +0800

    增加确认登录密码字段

--- commonapp/forms/userform.py
+++ commonapp/forms/userform.py
@@ -2,15 +2,20 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired()])
     password = PasswordField('密码', validators=[DataRequired()])
     remember_me = BooleanField('记住我', default=False)
-    submit = SubmitField('登录')
 
 
-class RegisterForm(FlaskForm):
-    pass
+class RegistForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired()])
+    realname = StringField('真实姓名',validators=[DataRequired()])
+    password = PasswordField('登录密码', validators=[DataRequired()])
+    password_again = PasswordField('确认登录密码', validators=[DataRequired()])
+    phone = StringField('电话号码', validators=[DataRequired()])
+    gender = StringField('性别')
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
